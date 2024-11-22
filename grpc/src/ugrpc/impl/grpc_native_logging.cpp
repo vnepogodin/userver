0567f9534d7bfb3fc9c41d1697e6784cb447c0de
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2024-11-23 01:52:30 +0400

    fix grpc: support gRPC >= 1.66 breaking changes

--- grpc/src/ugrpc/impl/grpc_native_logging.cpp
+++ grpc/src/ugrpc/impl/grpc_native_logging.cpp
@@ -2,6 +2,10 @@
 
 #include <stdexcept>
 
+#include <absl/log/globals.h>
+#include <absl/log/log.h>
+#include <absl/log/log_sink.h>
+#include <absl/log/log_sink_registry.h>
 #include <fmt/format.h>
 #include <grpc/support/log.h>
 
@@ -16,27 +20,34 @@ namespace ugrpc::impl {
 
 namespace {
 
-logging::Level ToLogLevel(::gpr_log_severity severity) noexcept {
+logging::Level ToLogLevel(::absl::LogSeverity severity) noexcept {
     switch (severity) {
-        case ::GPR_LOG_SEVERITY_DEBUG:
-            return logging::Level::kDebug;
-        case ::GPR_LOG_SEVERITY_INFO:
+        case ::absl::LogSeverity::kInfo:
             return logging::Level::kInfo;
-        case ::GPR_LOG_SEVERITY_ERROR:
+        case ::absl::LogSeverity::kFatal:
+            return logging::Level::kCritical;
+        case ::absl::LogSeverity::kError:
             [[fallthrough]];
-        default:
+        default: {
+            if (ABSL_VLOG_IS_ON(2)) {
+                return logging::Level::kDebug;
+            }
             return logging::Level::kError;
+        }
     }
 }
 
-::gpr_log_severity ToGprLogSeverity(logging::Level level) {
+::absl::LogSeverityAtLeast ToAbslLogSeverity(logging::Level level) {
     switch (level) {
         case logging::Level::kDebug:
-            return ::GPR_LOG_SEVERITY_DEBUG;
+            absl::SetVLogLevel("*grpc*/*", 2);
+            return ::absl::LogSeverityAtLeast::kInfo;
         case logging::Level::kInfo:
-            return ::GPR_LOG_SEVERITY_INFO;
+            absl::SetVLogLevel("*grpc*/*", -1);
+            return ::absl::LogSeverityAtLeast::kInfo;
         case logging::Level::kError:
-            return ::GPR_LOG_SEVERITY_ERROR;
+            absl::SetVLogLevel("*grpc*/*", -1);
+            return ::absl::LogSeverityAtLeast::kError;
         default:
             throw std::logic_error(fmt::format(
                 "grpcpp log level {} is not allowed. Allowed options: "
@@ -46,34 +57,40 @@ logging::Level ToLogLevel(::gpr_log_severity severity) noexcept {
     }
 }
 
-void LogFunction(::gpr_log_func_args* args) noexcept {
-    UASSERT(args);
-    const auto lvl = ToLogLevel(args->severity);
-    if (!logging::ShouldLog(lvl)) return;
+class NativeLogSink : public absl::LogSink {
+public:
+    NativeLogSink() { absl::AddLogSink(this); }
+    ~NativeLogSink() override { absl::RemoveLogSink(this); }
 
-    auto& logger = logging::GetDefaultLogger();
-    const auto location = utils::impl::SourceLocation::Custom(args->line, args->file, "");
-    logging::LogHelper(logger, lvl, location) << args->message;
+    void Send(const absl::LogEntry& entry) override {
+        const auto lvl = ToLogLevel(entry.log_severity());
+        if (!logging::ShouldLog(lvl)) return;
 
-    // We used to call LogFlush for kError logging level here,
-    // but that might lead to a thread switch (there is a coroutine-aware
-    // .Wait somewhere down the call chain), which breaks the grpc-core badly:
-    // its ExecCtx/ApplicationCallbackExecCtx are attached to a current thread
-    // (thread_local that is), and switching threads violates that, obviously.
-}
+        auto& logger = logging::GetDefaultLogger();
+        const auto location = utils::impl::SourceLocation::Custom(entry.source_line(), entry.source_filename(), "");
+        logging::LogHelper(logger, lvl, location) << entry.text_message();
+
+        // We used to call LogFlush for kError logging level here,
+        // but that might lead to a thread switch (there is a coroutine-aware
+        // .Wait somewhere down the call chain), which breaks the grpc-core badly:
+        // its ExecCtx/ApplicationCallbackExecCtx are attached to a current thread
+        // (thread_local that is), and switching threads violates that, obviously.
+    }
+};
 
 engine::Mutex native_log_level_mutex;
 auto native_log_level = logging::Level::kNone;
+std::optional<NativeLogSink> native_log_sink = std::nullopt;
 
 }  // namespace
 
-void SetupNativeLogging() { ::gpr_set_log_function(&LogFunction); }
+void SetupNativeLogging() { native_log_sink = std::make_optional<NativeLogSink>(); }
 
 void UpdateNativeLogLevel(logging::Level min_log_level_override) {
     std::lock_guard lock(native_log_level_mutex);
 
     if (utils::UnderlyingValue(min_log_level_override) < utils::UnderlyingValue(native_log_level)) {
-        ::gpr_set_log_verbosity(ToGprLogSeverity(min_log_level_override));
+        absl::SetMinLogLevel(ToAbslLogSeverity(min_log_level_override));
         native_log_level = min_log_level_override;
     }
 }
